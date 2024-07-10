0c67bb3c888b0f82e95b5ba5a834a00316e8e643
BELAYEL Salim <33290411+sb53systems@users.noreply.github.com>
2024-07-11 00:32:40 +0100

    Add files via upload

---
+++ Sources/Unit4.pas
@@ -0,0 +1,65 @@
+unit Unit4;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, jpeg, ExtCtrls, ShellAPI, Clipbrd, StdCtrls;
+
+type
+  TForm4 = class(TForm)
+    Image1: TImage;
+    StaticText3: TStaticText;
+    StaticText4: TStaticText;
+    StaticText1: TStaticText;
+    StaticText2: TStaticText;
+    StaticText5: TStaticText;
+    StaticText6: TStaticText;
+    StaticText7: TStaticText;
+    StaticText8: TStaticText;
+    StaticText9: TStaticText;
+    StaticText10: TStaticText;
+    StaticText11: TStaticText;
+    StaticText12: TStaticText;
+    Image2: TImage;
+    Label2: TLabel;
+    Edit1: TEdit;
+    Label1: TLabel;
+    StaticText13: TStaticText;
+    StaticText14: TStaticText;
+    StaticText15: TStaticText;
+    procedure Label1Click(Sender: TObject);
+    procedure Label2Click(Sender: TObject);
+    procedure Image2Click(Sender: TObject);
+  private
+    { Déclarations privées }
+  public
+    { Déclarations publiques }
+  end;
+
+var
+  Form4: TForm4;
+
+implementation
+
+{$R *.dfm}
+
+procedure TForm4.Label1Click(Sender: TObject);
+var
+  URL: string;
+begin
+  URL := 'https://github.com/sb53systems/G-Code-Flow-Temperature-Controller';
+  ShellExecute(0, 'open', PChar(URL), nil, nil, SW_SHOWNORMAL);
+end;
+
+procedure TForm4.Label2Click(Sender: TObject);
+begin
+  showmessage('Use my Email to Manualy Tip me in PayPal. Thank You !');
+end;
+
+procedure TForm4.Image2Click(Sender: TObject);
+begin
+  showmessage('Use my Email to Manualy Tip me in PayPal. Thank You !');
+end;
+
+end.
