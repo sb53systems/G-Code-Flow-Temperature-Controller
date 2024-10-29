7fc38143129f9478920805c9c41fa21d0b49bcd9
BELAYEL Salim <sb53systems@gmail.com>
2024-10-29 22:00:31 +0100

    V1.1

--- Source/V1.1/Project1.dpr
+++ Source/V1.1/Project1.dpr
@@ -8,7 +8,8 @@ uses
   Unit4 in 'Unit4.pas' {Form4},
   Unit5 in 'Unit5.pas' {Form5},
   Unit6 in 'Unit6.pas' {Form6},
-  Unit7 in 'Unit7.pas' {Form7};
+  Unit7 in 'Unit7.pas' {Form7},
+  Unit8 in 'Unit8.pas' {Form8};
 
 {$R *.res}
 
@@ -22,5 +23,7 @@ begin
   Application.CreateForm(TForm5, Form5);
   Application.CreateForm(TForm6, Form6);
   Application.CreateForm(TForm7, Form7);
+  Application.CreateForm(TForm7, Form7);
+  Application.CreateForm(TForm8, Form8);
   Application.Run;
 end.
